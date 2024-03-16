78248e1c4c7d0bba86b4d149500d3de39c0bd7ee
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-03-16 13:05:06 +0100

    deleted comment

--- main.c
+++ main.c
@@ -3,69 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-/*
-    Create a C program that reads INI files using only the C standard library
-    You are allowed to work in pairs
-    Share the source code through eKursy
-    Tips:
-        Compile with -Wall -Wextra flags and resolve all warnings
-        Use Valgrind to check for any memory errors or leaks.
-
-✔️ Requirements for 3.0
-
-  ✔️  The program needs to receive two command line parameters in this format:
-
-    $ ./program PATH-TO-INI-FILE.ini section.key
-
-    For example:
-
-    $ ./program test.ini data.velocity
-
-✔️    The program should display the value of the key in the specified section.
-
-    For this example, the program needs to show the value associated with the velocity key in the [data] section.
-  
-✔️    The program can work with the following constraints:
-✔️        Number of sections is less than 30.
-✔️        Length of section name is less than 30.
-✔️        Length of the key is less than 30.
-✔️        Length of the value is less than 30.
-
-✔️ Requirements for 3.5
-
-✔️    The program should be able to identify invalid identifiers in an INI file, both in section names and keys (but not values).
-✔️        Invalid identifiers are those that contain any characters other than letters, digits, and dashes (-).
-✔️        If an invalid identifier is found, the program should show a message and halt.
-✔️    The program needs to identify when a section is missing and display a specific message like Failed to find section [data].
-✔️    The program must identify when a key is missing in a section that exists, displaying a message such as Failed to find key "velocity" in section [data].
-
-✔️ Requirements for 4.0
-
-✔️   Define C structures for storing key-value pairs and sections.
-✔️    Use these structures to read the entire INI file and search for the section.key specified by the user.
-
-✔️ Requirements for 4.5
-
-✔️    The limitations specified in the 3.0 requirements are no longer relevant.
-✔️    As a result, the program needs to be able to handle arbitrary INI files containing section names, keys, or values of any length without being restricted by compile-time limits.
-✔️    The program should parse the INI file just once and adjust dynamically to accommodate data of different sizes.
-
-Requirements for 5.0
-
-✔️    The program needs to differentiate between strings and numbers for values.
-
-✔️    The program should be able to process simple mathematical expressions provided through the command line, for example:
-
-    $ ./program PATH-TO-INI-FILE.ini expression "distance.velocity * travel.time"
-
-✔️    The program has the following rules for evaluating expressions:
-✔️        For numbers, it should support addition (+), subtraction (-), multiplication (*), and division (/).
-✔️        For strings, it should support concatenation (+).
-✔️        An expression containing operands of different types is considered invalid.
-✔️        The use of operators -, *, and / with string operands is also invalid.
-✔️        In such cases, an error message should be displayed.
-*/
-
 
 struct Tuple
 {
@@ -196,10 +133,12 @@ char *getValue(struct Section *data, int dataLength, char *sectionKey)
   return "Section not found.";
 }
 
+
 char *evaluate(struct Section* data, int dataLength, char* expression){
   char* key1 = malloc(sizeof(char) * strlen(expression));
   char* key2 = malloc(sizeof(char) * strlen(expression));;
   char operator;
+  
   if(sscanf(expression, "%[^ ] %c %[^ ]", key1, &operator, key2) == 3){
     char* value1 = getValue(data, dataLength, key1);
     char* value2 = getValue(data, dataLength, key2);
